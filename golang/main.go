abb2ce7c72b89f368afd428e71bfbb9c6cbd2973
Keynandz <ndhsti2980@gmail.com>
2023-08-07 15:11:41 +0700

    Menambahkan backend golang

---
+++ golang/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"go-collab/cmd/handlers"
+	"go-collab/cmd/storage"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	storage.InitDB()
+
+	e.POST("/register", handlers.CreateAkun)
+	e.Logger.Fatal(e.Start(":5000"))
+}
