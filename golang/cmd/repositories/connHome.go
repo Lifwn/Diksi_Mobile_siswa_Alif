d1123cce78965dc58a5085dab7b5cde3923e7394
Keynandz <ndhsti2980@gmail.com>
2023-08-09 09:43:02 +0700

    Reset backend

--- golang/cmd/repositories/connHome.go
+++ golang/cmd/repositories/connHome.go
@@ -12,13 +12,13 @@ func GetAkunByID(identifier string) (models.Akun, error) {
 	db := storage.GetDB()
 
 	sqlStatement := `
-		SELECT username FROM akun WHERE username = $1 OR email = $1
+		SELECT id, username, email, password FROM akun WHERE username = $1 OR email = $1
 `
 
 	row := db.QueryRow(sqlStatement, identifier)
 
 	user := models.Akun{}
-	err := row.Scan(&user.Username)
+	err := row.Scan(&user.Id, &user.Username, &user.Email, &user.Password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return user, fmt.Errorf("user not found")
